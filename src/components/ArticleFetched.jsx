e65733635c37787997ea4c73fb0b75c29a5b8dbc
clint911 <clingyking007@gmail.com>
2023-06-15 23:12:56 +0300

    Adding the first part of the admin template

--- src/components/ArticleFetched.jsx
+++ src/components/ArticleFetched.jsx
@@ -5,7 +5,7 @@ export default function ArticleFetched() {
   //handles the fetching of json data from the data directory returns a json object that holds the data
   async function fetchArticle() {
     let fetchedArticle = await fetch("../data/mockingServer.json");
-
+    console.log(fetchedArticle);
   }
 }
 
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function AdminPage() {
+  return (
+    <div className="AdminPage">
+      <h1>This is the page where you can create and delete articles</h1>
+      <div className="Administering the article">
+        <h2>Place Your Article Heading here</h2>
+      </div>
+    </div>
+  )
+}
+
+export default AdminPage;
